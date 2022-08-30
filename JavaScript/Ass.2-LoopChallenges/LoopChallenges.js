9a08b578406b332cd318de766dc69a37b6c31f10
Osama Tbaileh <osama.tb02@gmail.com>
2022-08-30 12:34:33 +0300

    LoopChallenges

--- JavaScript/Ass.2-LoopChallenges/LoopChallenges.js
+++ JavaScript/Ass.2-LoopChallenges/LoopChallenges.js
@@ -1,7 +1,6 @@
 // Print odds 1-20
-
 function printodds(){
-    for ( var i=1 ; i<20 ; i++ ) {
+    for (var i=1 ; i<20 ; i++) {
         if(i%2==1){
             console.log(i);
         }
@@ -9,8 +8,56 @@ function printodds(){
 }
 printodds()
 
-// Decreasing Multiples of 3
 
+
+
+
+// Decreasing Multiples of 3
 function decreasingmultiples(){
-    for( var j=100 ; j>=0 ; j+=Decreasing Multiples)
+    for(var j=100 ; j>=0 ; j--){
+        if(j%3==0){
+            console.log(j);
+        }
+    }
 }
+decreasingmultiples()
+
+
+
+
+
+// Print the sequence
+function printthesequence(){
+    for(z=4 ; z>-4 ; z-=1.5){
+        console.log(z);
+    }
+}
+printthesequence()
+
+
+
+
+
+// Sigma 
+function sigma(){
+    var sum=0;
+    for(var x=1 ; x<=100 ; x++){
+        sum=sum+x;
+    }
+    console.log(sum);
+}
+sigma()
+
+
+
+
+
+// Factorial
+function factorial(){
+    var product=1;
+    for(var y=1 ; y<=12 ; y++){
+        product*=y;
+    }
+    console.log(product);
+}
+factorial()
