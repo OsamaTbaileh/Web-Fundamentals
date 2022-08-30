27280969b1507372af851fe95993aac399dd39d7
Osama Tbaileh <osama.tb02@gmail.com>
2022-08-30 10:44:48 +0300

    Loop Challenges

---
+++ JavaScript/Ass.2-LoopChallenges/LoopChallenges.js
@@ -0,0 +1,16 @@
+// Print odds 1-20
+
+function printodds(){
+    for ( var i=1 ; i<20 ; i++ ) {
+        if(i%2==1){
+            console.log(i);
+        }
+    }
+}
+printodds()
+
+// Decreasing Multiples of 3
+
+function decreasingmultiples(){
+    for( var j=100 ; j>=0 ; j+=Decreasing Multiples)
+}
